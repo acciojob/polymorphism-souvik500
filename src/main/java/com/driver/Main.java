9e0d2219625cbe97f9b12f9f423f0aeb996342bd
souvikadhikary <souvik500>
2023-02-09 01:09:01 +0530

    Encapsulation file

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,10 +4,10 @@ public class Main {
     public class Product
     {
         public int product(int x, int y) {
-            return x+y;
+            return x*y;
         }
         public int product(int x, int y, int z) {
-            return x-y+z;
+            return x*y*z;
         }
         public double product(double x, double y) {
             return x*y;
